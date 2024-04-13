7f99c6988da7dd1e9e2a70742edbcb885c45a911
duongbuibd <duongbuibd@gmail.com>
2024-04-13 19:54:50 +0700

    refactor: config envVars

---
+++ src/config/config.ts
@@ -0,0 +1,28 @@
+import Joi, { valid } from 'joi';
+import 'dotenv/config';
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    PORT: Joi.number().default(5555),
+    MONGODB_URL_DEV: Joi.string().required().description('Local Mongo DB'),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+console.log('envVars.PORT', envVars.PORT);
+const config = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  mongoose: {
+    url: envVars.MONGODB_URL_CLOUD || envVars.MONGODB_URL_DEV,
+    options: {
+      dbName: 'unicorn',
+    },
+  },
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import app from './app';
 import mongoose from 'mongoose';
+import config from './config/config';
 
-const PORT = process.env.PORT || 5555;
+const PORT = config.port;
 
 let server: any;
-mongoose.connect('mongodb://localhost:27017').then(() => {
-  console.log('Connected to MongoDB');
-  server = app.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}`);
+mongoose
+  .connect(`${config.mongoose.url}`, {
+    dbName: config.mongoose.options.dbName,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    server = app.listen(PORT, () => {
+      console.log(`Listening to port ${PORT}`);
+    });
   });
-});
 
 const exitHandler = () => {
   if (server) {
