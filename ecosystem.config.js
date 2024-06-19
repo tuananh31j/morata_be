f2ac69fd6219d9aa6dd8f0b747b647e7a0b4b2b2
duongbv <duong.bv@eastgate-software.com>
2024-06-19 19:22:52 +0700

    config: config ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'morata_be',
+      script: 'npx',
+      args: "nodemon --exec 'ts-node' ./src/index.ts", // Ensure the path to your main file is correct
+      interpreter: 'none',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};
