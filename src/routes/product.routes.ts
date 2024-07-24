8f42bbf0e5b775cc0557900140f262e20443502e
Tuan Anh <tuananh31j@gmail.com>
2024-07-24 20:28:49 +0700

    chore: restore get all product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { productController } from '@/controllers';
 import { validateObjectId } from '@/validation';
 import upload from '@/middlewares/multerMiddleware';
+import { addProductValidation } from '@/validation/product';
 import { authenticate } from '@/middlewares/authenticateMiddleware';
 import { authorize } from '@/middlewares/authorizeMiddleware';
 import { ROLE } from '@/constant/allowedRoles';
 
 const router = Router();
 
+router.get('/all', productController.getAllProducts);
 router.get('/latest', productController.getTopLatestProducts);
 router.get('/deals', productController.getTopDealsOfTheDay);
 router.get('/reviews', productController.getTopReviewsProducts);
@@ -27,7 +29,6 @@ router.post(
     // [addProductValidation],
     productController.createNewProduct,
 );
-
 router.patch(
     '/:id',
     [authenticate, authorize(ROLE.ADMIN), validateObjectId],
@@ -37,5 +38,6 @@ router.patch(
     ]),
     productController.updateProduct,
 );
+router.delete('/:id', [authenticate, authorize(ROLE.ADMIN), validateObjectId], productController.deleteProduct);
 
 export default router;
