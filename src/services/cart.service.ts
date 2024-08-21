97d4537765ef22d73e13290106a06bfc98792014
Tuan Anh <tuananh31j@gmail.com>
2024-08-22 00:50:07 +0700

    chore: restore cart is not active

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -18,10 +18,7 @@ export const getCartByUser = async (req: Request, res: Response, next: NextFunct
         .lean();
     if (!cartUser) throw new NotFoundError('Not found cart or cart is not exist.');
     const filteredProducts = cartUser.items.filter(
-        (item) =>
-            (item.productVariation as any).isActive !== false &&
-            (item.productVariation as any).stock > 0 &&
-            (item.productVariation as any).productId.isHide !== true,
+        (item) => (item.productVariation as any).stock > 0 && (item.productVariation as any).productId.isHide !== true,
     );
     cartUser.items = filteredProducts;
     return res
